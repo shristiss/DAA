929549abd1d6e447cd891986f80bb6d683add49b
Shristi Sethiya <85256662+shristiss@users.noreply.github.com>
2022-06-19 02:15:34 +0530

    Update q1.cpp

--- Week8/Ques1/q1.cpp
+++ Week8/Ques1/q1.cpp
@@ -1,49 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-void prims(vector<pair<int,int>>adj[],int v)
+vector<pair<int,int>>g[5];
+void prims(int e,int src)
 {
+    vector<bool>vis(5,false);
+  
     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-    int src=0;
-    vector<int>key(v,1e6+10);
-    vector<int>parent(v,-1);
-    vector<bool>vis(v,false);
     pq.push({0,src});
-    key[src]=0;
+    int ans=0;
     while(!pq.empty())
     {
-        int u=pq.top().second;
+        int node=pq.top().second;int weight=pq.top().first;
         pq.pop();
-        if(vis[u])continue;
-        vis[u]=true;
-        for(auto x:adj[u])
+        if(vis[node])continue;
+        ans+=weight;
+        cout<<ans<<" ";
+        
+        
+        vis[node]=true;
+        for(auto child:g[node])
         {
-            int v=x.first;
-            int weight=x.second;
-            if(!vis[v] && key[v]>weight)
+            if(!vis[child.first])
             {
-                key[v]=weight;
-                pq.push({key[v],v});
-                parent[v]=u;
+                pq.push({child.second,child.first});
             }
         }
+        
     }
-    for(int i=1;i<v;i++)
-    {
-        cout<<parent[i]<<" "<<i<<" "<<key[i]<<endl;
-    }
+    cout<<ans;
     
 }
 int main()
 {
-    int n,e;
-    cin>>n>>e;
-    vector<pair<int,int>>adj[n];
-    for(int i=0;i<e;i++)
-    {
+    cout<<"enter edges";
+    int e;
+    cin>>e;
+    for(int i=0;i<e;i++){
         int u,v,wt;
         cin>>u>>v>>wt;
-        adj[u].push_back({v,wt});
-        adj[v].push_back({u,wt});
+        g[u].push_back({v,wt});
+        g[v].push_back({u,wt});
     }
-    prims(adj,n);
+    int src;
+    cout<<"enter source";
+    cin>>src;
+prims(e,src);
+    return 0;
 }
